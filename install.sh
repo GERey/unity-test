6c40e7d5e69b551b16925ff48db0db24fc450913
Sean Heuer <sean@digitalrhomb.us>
2016-07-27 18:07:38 -0700

    Update editor version

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-curl -o Unity.pkg http://download.unity3d.com/download_unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/29055738eb78/MacEditorInstaller/Unity-5.3.6f1.pkg
 sudo installer -dumplog -package Unity.pkg -target /
